cb70e2c35a9b7d653a6ae15e148c526320083349
Iorozcop <iorozcopuerto@gmail.com>
2021-04-30 13:25:31 +0200

    componente convertido a custom hook

--- src/App.js
+++ src/App.js
@@ -2,28 +2,46 @@ import React,{useState,useEffect} from "react";
 import "./style.css";
 
 export default function App() {
-  const[colour,setColour]= useState();
-  const[state,setState]=useState(false);
 
-  useEffect(()=>{
-    if(state){
-      setColour('yellow');
-    }else{
-      setColour('')
-    }
-    console.log(colour);
-    console.log(state);
-  },[state])
+  function useHightlight(color, estado){
+    const[colour,setColour]= useState();
+    const[state,setState]=useState(estado);
 
-  return (
-    <>
-    <div style={{display:'flex',margin:'20px'}}>
-      <button onClick={()=>setState(!state)}>
-      {!state ? "ON" : "OFF"}</button>
+    useEffect(()=>{
+      if(state){
+        setColour('yellow');
+      }else{
+        setColour('')
+      }
+      console.log(colour);
+      console.log(state);
+    },[state])
+
+    return (
+      <>
+      <div style={{display:'flex',margin:'20px'}}>
+        <button onClick={()=>setState(!state)}>
+        {!state ? "ON" : "OFF"}</button>
+      </div>
+      <div style={{width:'40px',background:colour,margin:'20px'}}>=^.^=</div>
+      </>
+    );
+  }
+
+  const off = useHightlight('',false);
+  const on = useHightlight('',true);
+
+  return(
+    <div style={{display:'flex'}}>
+      <div class="lightcat">
+        Turn on the cat {off}
+      </div>
+      <div class="lightcat">
+        Turn off the cat {on}
+      </div>
     </div>
-    <div style={{width:'40px',background:colour,margin:'20px'}}>=^.^=</div>
-    </>
-  );
+  )
+  
 }
 
 
